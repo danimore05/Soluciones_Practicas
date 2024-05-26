affc60282c41e18d7845f656afcc2e91838fca71
danimore05@gmail.com <morelatod.86@gmail.com>
2024-05-25 21:41:51 -0300

    commit 5

---
+++ Estructura_Iterativa_Ejercicio1.py
@@ -0,0 +1,7 @@
+#Motrar numeros entre el 0 y el numero ingresado por el usuario
+
+NumIngresadoporUsuario= int(input("Escriba un numero: "))
+
+for i in range (NumIngresadoporUsuario+1):
+    print (i)
+    
